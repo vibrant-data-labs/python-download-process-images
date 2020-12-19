0066e6563180fd8fefeaf56a6643fc22ac06f8d6
ericberlow <ericberlow@gmail.com>
2020-12-19 12:37:40 -0800

    Feature: added functions that use and return dataframes not csv
    row results are added to dictionary not list - and the dictionary is mapped to the dataframe to make new columns- this fixes the error of list length not matching.

--- imagetools.py
+++ imagetools.py
@@ -10,6 +10,7 @@ import configparser
 import boto3
 import pandas as pd
 from botocore.exceptions import ClientError
+import mimetypes
 from csv import reader, writer
 
 ### Config Setup ###
@@ -31,7 +32,10 @@ S3_CLIENT = boto3.client(
     region_name=REGION
 )
 
+##############################
 ### Download Functions ###
+##############################
+
 def get_image_name(name, sub='-', ext=''):
     # Return a clean image name from an input name
     chars = " &._()|,'\"\\/:"
@@ -91,8 +95,71 @@ def download_images(csv_file="sample_image_list.csv",image_dir=IMAGE_DIR,as_png=
             local.append(f'ERROR - {e}')
     data["local"] = local
     data.to_csv(csv_file,index=False)
+    
+    
+# download images from pandas dataframe
+def download_images_df(df, image_dir=IMAGE_DIR,as_png=True):
+    '''
+    description: 
+        download images from image url to local folder 
+        and add new column with path/filename to the local image file
+        the dataframe must have columns 'name' and 'image_url'
+    ----------
+    Parameters:
+    df : datafrane with columns 'name', 'image_url' 
+    image_dir : local directory where to download images
+    as_png : if true will convert all images to png
+    ----------
+    Returns: datframe with new column of local image filename
 
+    '''
+    
+    requests_cache.install_cache()
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/540.0 (KHTML,like Gecko) Chrome/9.1.0.0 Safari/540.0"
+    }
+    
+    print("downloading images to local directory")
+    # make output directory if it doesn't exist
+    os.makedirs(image_dir, exist_ok=True)
+
+    d = {} # dictionary to hold results {name:filename} pairs
+
+    # loop through dataframe
+    for row in df.itertuples():
+        print("downloading image for %s" %row.name)
+        try:       
+            image_url = row.image_url
+            if image_url:
+                filename = get_image_name(row.name)
+                r = requests.get(image_url, timeout=10, headers=headers)               
+                ext = image_ext_or_none(r)
+                
+                if ext:                                          
+                    file_path = os.path.join(image_dir, filename + ext)
+                    
+                    with open(file_path, 'wb') as f:
+                        f.write(r.content)
+                    
+                    if as_png:
+                        if ext != '.png':
+                            file_path = change_image_type(file_path, '.png')
+                            
+                    d[row.name]=file_path
+            else:
+                d[row.name] = "no url"
+                
+        except:
+            d[row.name]="url broken" # no results found
+    df['filename'] = df['name'].map(d) # map filename value to name as new column in dataframe
+    df['filename'].fillna("other url error", inplace=True)
+    return df
+      
+
+##############################
 ### Processing Functions ###
+##############################
+
 def process_images(image_dir=IMAGE_DIR,change_type=None,
                    resize=False,width=200,height=200,
                    grayscale=False,padding=False,
@@ -184,10 +251,18 @@ def add_padding(name, width, height, color=""):
     cmd = f'convert -size {size} xc:{color} {name} -gravity center -composite {name}'
     os.system(cmd)
 
+
+##############################
 ### S3 Functions ###
-# uploads a directory of images to s3
+##############################
+
+# uploads a directory of images to s3 from csv file
 def upload_images(csv_file="sample_image_list.csv",image_dir=IMAGE_DIR,s3_bucket=BUCKET):    
-    # upload images to s3 bucket and add image enpoint url to file
+    '''
+    upload images to s3 bucket and add image enpoint url to file    
+    '''
+
+    print("uploading images to AWS s3 budket: %s" %s3_bucket)
     # create bucket if it doesn't exist
     S3_CLIENT.create_bucket(Bucket=s3_bucket, ACL='public-read')
 
@@ -199,7 +274,7 @@ def upload_images(csv_file="sample_image_list.csv",image_dir=IMAGE_DIR,s3_bucket
 
     # loop through csv
     for row in data.itertuples():
-        filename = row.local
+        filename = row.filename
         if "ERROR" not in filename:
             image = filename.split('/')[1]
             # try to upload the file to s3
@@ -216,3 +291,84 @@ def upload_images(csv_file="sample_image_list.csv",image_dir=IMAGE_DIR,s3_bucket
             s3_urls.append('ERROR - not uploaded')
     data["s3_url"] = s3_urls
     data.to_csv(csv_file,index=False)
+
+
+# upload a directory of images to s3 from dataframe
+def upload_images_df(df,image_dir=IMAGE_DIR,s3_bucket=BUCKET):    
+    '''
+    Description:  
+        upload images to s3 bucket
+        create bucket if it doesn't exist
+        add column to dataframe with image endpoint url
+    ----------
+    Parameters:
+        df : pandas dataframe with columns "name", "filename"
+        image_dir : directory path of local image files to upload
+        s3_bucket : name of s3 bucket where to upload images.
+
+    Returns: dataframe with extra column of s3 endpoint url
+
+    '''
+    print("uploading images to s3")
+    
+    S3_CLIENT.create_bucket(Bucket=s3_bucket, ACL='public-read')
+
+    # create s3 endpoint column
+    s3_urls_dict = {} # dictionary to hold results {name: s3_url}
+    
+    # remove rows that have no filepath or error name
+    error_rows = df['filename'].isin(["", "no url", "url broken", "other url error"])
+    df_trimmed = df[~error_rows].reset_index(drop=True)
+
+    # loop through dataframe
+    for row in df_trimmed.itertuples():
+        print("uploading image for %s to s3 bucket: %s " %(row.name, s3_bucket))
+        filename = row.filename
+        mime_type = mimetypes.guess_type(filename)[0] # detect image type (png, jpg, svg, etc)
+        if "ERROR" not in filename: # double check no other errors to skip
+            image = filename.split('/')[1]
+            # try to upload the file to s3
+            try:
+                S3_CLIENT.upload_file(
+                    filename, s3_bucket, image,
+                    ExtraArgs={'ACL': 'public-read', # make the endpoint public
+                               'ContentType': mime_type}) #  image type necessary for display instead of download 
+                s3_urls_dict[row.name]= (f'https://{s3_bucket}.s3.amazonaws.com/{image}')
+            except ClientError as e:
+                s3_urls_dict[row.name]=(f'ERROR - {e}')
+            except FileNotFoundError as e:
+                s3_urls_dict[row.name]=(f'ERROR - {e}')
+        else:
+            s3_urls_dict[row.name]=('ERROR - not uploaded')
+    df["s3_url"] = df['name'].map(s3_urls_dict) # map filename value to name as new column in dataframe
+    df['s3_url'].fillna("", inplace=True)
+    return df
+    
+
+##################################
+### Example test script
+##################################
+    
+if __name__ == '__main__':
+   image_directory = "data/images"
+   # read file  - doesn't matter if it's csv or excel.
+   df = pd.read_excel('data/test.xlsx')[['Recipient', 'Logo_URL']] # subset columns with image name and url
+   df.columns = ['name', 'image_url'] # rename columns for processing - must have these names 
+
+   # download images to local directory and update dataframe with filename column
+   df = download_images_df(df, image_dir=image_directory, as_png=True) # convert all to png if possible
+
+   # process all images in the local directory (e.g. resize, convert to grey)
+   process_images(image_dir=image_directory, resize=True,width=200,height=200,
+                          grayscale=False,padding=False,
+                          padding_width=100,padding_height=100)
+   
+   # upload images in the local directory to s3 and update dataframe with s3 endpoint
+   df = upload_images_df(df, image_dir=image_directory)
+   
+   # add original column names and s3 url cleaned of errors
+   df['Recipient'] = df['name']
+   df['Logo_URL'] = df['s3_url'].apply(lambda x: "" if "ERROR" in x else x) # replace error messages with empty string
+   
+   # write processed file with added columns
+   df.to_excel('data/test_processed.xlsx', index=False, encoding = 'utf-8-sig')
